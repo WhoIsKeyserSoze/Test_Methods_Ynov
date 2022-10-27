e941715f06addaceaea47ab5e2208f7313215807
WhoIsKeyserSoze <gaby.foix@gmail.com>
2022-10-27 15:46:01 +0200

    Add implementation for added dwarfs per type in unsafe zone | tests goes green

--- python/src/digging_estimator.py
+++ python/src/digging_estimator.py
@@ -71,7 +71,7 @@ class Team:
     def calculate_nb_washers_guard_and_guard_managers(self, nb_washers, nb_guards, nb_guard_managers):
         new_nb_washers = math.ceil(
             (self.miners + self.healers + self.smithies + self.inn_keepers + self.lighters + nb_guards +
-             nb_guard_managers) / 10)
+             nb_guard_managers + self.protectors) / 10)
         new_nb_guards = math.ceil((self.miners + self.healers + self.smithies + self.lighters + new_nb_washers) / 3)
         new_nb_guard_managers = math.ceil(new_nb_guards / 3)
 
@@ -119,25 +119,35 @@ class DiggingEstimator:
         night_team = composition.night_team
 
         if day_team.miners > 0:
+            if presence_of_goblins:
+                day_team.add_worker(WORKER.PROTECTORS, 2)
             day_team.add_worker(WORKER.HEALERS)
             day_team.add_worker(WORKER.SMITHIES, 2)
 
-            nb_inn_keepers_to_add = math.ceil((day_team.miners + day_team.healers + day_team.smithies) / 4.0) * 4
+            nb_inn_keepers_to_add = math.ceil((day_team.miners
+                                               + day_team.healers
+                                               + day_team.smithies
+                                               + day_team.protectors) / 4.0) * 4
             day_team.add_worker(WORKER.INN_KEEPERS, nb_inn_keepers_to_add)
 
             nb_inn_washers_to_add = math.ceil((day_team.miners + day_team.healers + day_team.smithies +
-                                               day_team.inn_keepers) / 10.0)
+                                               day_team.inn_keepers + day_team.protectors) / 10.0)
             day_team.add_worker(WORKER.WASHERS, nb_inn_washers_to_add)
 
         if night_team.miners > 0:
+            if presence_of_goblins:
+                night_team.add_worker(WORKER.PROTECTORS, 2)
             night_team.add_worker(WORKER.HEALERS)
             night_team.add_worker(WORKER.SMITHIES, 2)
-            night_team.add_worker(WORKER.LIGHTERS, night_team.miners + 1)
+            night_team.add_worker(WORKER.LIGHTERS, night_team.miners + 1 + night_team.protectors)
 
-            nb_inn_washers_to_add_for_night_team = math.ceil(
-                (night_team.miners + night_team.healers + night_team.smithies +
-                 night_team.lighters) / 4.0) * 4
-            night_team.add_worker(WORKER.INN_KEEPERS, nb_inn_washers_to_add_for_night_team)
+            nb_inn_keepers_to_add_for_night_team = math.ceil(
+                (night_team.miners
+                 + night_team.healers
+                 + night_team.smithies
+                 + night_team.lighters
+                 + night_team.protectors) / 4.0) * 4
+            night_team.add_worker(WORKER.INN_KEEPERS, nb_inn_keepers_to_add_for_night_team)
 
         nb_washers, nb_guards, nb_guard_managers = night_team.calculate_nb_washers_guard_and_guard_managers(0, 0, 0)
         night_team.add_worker(WORKER.WASHERS, nb_washers)
